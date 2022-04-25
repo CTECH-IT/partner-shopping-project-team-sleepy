777a9f258db03441978e88db34efcc6fc32f089d
= <=>
2022-04-25 10:01:44 -0500

    fixed bugs relating to email

---
+++ scripts/checklist.js
@@ -0,0 +1,63 @@
+(function (window) {
+    'use strict';
+    let App = window.App || {};
+    let $ = window.jQuery;
+    function CheckList(selector) {
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+        this.$element = $(selector);
+        if (this.$element.length ===0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    CheckList.prototype.addRow = function (shoppingOrder) {
+        this.removeRow(shoppingOrder.emailAddress);
+        var rowElement = new Row(shoppingOrder);
+        this.$element.append(rowElement.$element);
+    }
+
+    CheckList.prototype.removeRow = function (email) {
+        this.$element
+        .find('[value="' + email + '"]')
+        .closest('[data-shopping-order="checkbox"]')
+        .remove();
+    };
+
+    CheckList.prototype.addClickHandler = function (func) {
+        this.$element.on('click', 'input', function (event) {
+            var email = event.target.value;
+            this.removeRow(email);
+            func(email);
+        }.bind(this));
+    };
+
+    function Row(shoppingOrder) {
+        let $div = $('<div></div>', {
+            'data-shopping-order': 'checkbox', 'class': 'checkbox'
+        });
+        let $label = $('<label></label>');
+
+        let $checkbox = $('<input></input>', {
+            type: 'checkbox',
+            value: shoppingOrder.emailAddress
+        });
+
+        let description = shoppingOrder.shopping + ', ';
+        if (shoppingOrder.flavor) {
+            description += shoppingOrder.flavor + ' ';
+        }
+        description += shoppingOrder.shopping + ', ';
+        description += ' (' + shoppingOrder.emailAddress + ')';
+        description += ' [' + shoppingOrder.strength + 'x]';
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+
+        this.$element = $div;
+    }
+    App.CheckList = CheckList;
+    window.App = App;
+})(window);
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
-// Put your JavaScript here
+(function (window) {
+    'use strict';
+
+    const FORM_SELECTOR = '[data-shopping-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-shopping-order="checklist"]';
+    const SERVER_URL = 'https://saturn.rochesterschools.org:8080/json';
+
+    let App = window.App;
+    let Truck = App.Truck;
+    let DataStore = App.DataStore;
+    let RemoteDataStore = App.DataStore;
+    let FormHandler = App.FormHandler;
+    let CheckList = App.CheckList;
+    let Validation = App.Validation;
+
+    let remoteDS = new RemoteDataStore(SERVER_URL);
+
+    let myTruck = new Truck('12345', remoteDS);
+    window.myTruck = myTruck;
+
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
+
+    let formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler(function (data) {
+        console.log(data);
+        myTruck.createOrder(data);
+        checkList.addRow(checkList, data);
+    })
+
+    
+
+    console.log(formHandler);
+
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
+
+})(window);
